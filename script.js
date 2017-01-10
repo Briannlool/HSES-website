d14efe9d9752dac7f02ad53b807910405e64f94d
Brian Chan <brian.chan080@gmail.com>
2017-01-10 22:36:57 +0000

    added about

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* global $ */ 
-$("button").click(function(){
+$("#ik").click(function(){
     alert("Why did you choose this school when your child can do so much better? lmao");
+});
+
+$("#memes").click(function(){
+    alert("Go to this website for more dank memes that truly represent our school: https://www.reddit.com/r/dankmemes/");
 });
